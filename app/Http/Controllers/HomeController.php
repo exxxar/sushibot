8c8a7aecbf491cef21b88ca8582f2f48a024f3fd
exxxar <exxxar@gmail.com>
2020-02-17 22:09:41 +0300

    добавление логина через вк

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use ATehnix\VkClient\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -22,9 +23,17 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $auth = new Auth('6803875', 'l1rMo05qkLGM8BSh5KbQ', 'http://isushi.herokuapp.com/admin');
+
+        $token = null;
+
+        if ($request->has("code")){
+            $token = $auth->getToken($request->get['code']);
+        }
+
+        return view('home',compact("auth","token"));
     }
 
     public function searchAjax(Request $request)
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    You are logged in!
+                        @isset($token)
+                            <p>{{$token}}</p>
+                        @endisset
+                        <hr>
+                        @isset($auth)
+                            <a class="btn btn-primary" href="{{$auth->getUrl()}}">Войдите в ВК<a>
+                        @endisset
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
