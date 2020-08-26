752198172331c30e3e5e7018002b19f62b31b37c
exxxar <exxxar@gmail.com>
2020-08-26 11:46:26 +0300

    fixes

--- resources/views/main/partials/header_bg.blade.php
+++ resources/views/main/partials/header_bg.blade.php
@@ -10,7 +10,7 @@
             <div class="col-lg-8">
                 <div class="welcome_content wow slideInUp text-center" style="visibility: visible;">
                     <h1>Добро пожаловать в<br>ISUSHI & PIZZA</h1>
-                    <h3>Не знаете где быстро заказать аппетитные и качественные роллы?</h3>
+                    <h3>Не знаете где быстро заказать аппетитные и качественные роллы или вкуснейшую пиццу?</h3>
                     <a href="#menu_grid" class="chopcafe_btn chopcafe_btn">Наше меню</a>
 
                     <a href="#callback" data-toggle="modal" data-target="#callbackModal" class="chopcafe_btn chopcafe_btn_2">Оформить заказ по телефону</a>
